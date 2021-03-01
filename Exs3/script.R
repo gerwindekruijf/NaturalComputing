9a20d2341dc600cc9e2aea94c01838cc3b5c8878
Gerwin de Kruijf <gerwindekruijf@gmail.com>
2021-03-01 13:30:58 +0100

    update script

--- Exs3/script.R
+++ Exs3/script.R
@@ -1,5 +1,18 @@
 # Olivier Brahma, Gerwin de Kruijf, Dirren van Vlijmen
 
 # library(AUC)
+# load data scores 
 
-# 
+# Load scores_english and scores_tagalog with 
+# 1. Import dataset
+# 2. From text base
+# 3. Import the two datasets
+merged = merge(x = scores_english, y = scores_tagalog, by="V1", all=TRUE)
+
+# Sort dataset
+sorted = merged[order(merged$V1),]
+
+
+
+
+
