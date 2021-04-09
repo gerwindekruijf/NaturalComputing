9893ee247a5195c0d73dafd3b17855f3be0ddb48
Gerwin de Kruijf <gerwindekruijf@gmail.com>
2021-04-09 13:20:16 +0200

    ex2 van ensemble learning

---
+++ Exs5/ex2.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import math as m
+import numpy as np
+
+
+# Python version >= 3.8
+def calc_cumbinprob(n, p):
+    result = 0
+
+    x = m.floor(n / 2) + 1
+
+    for k in range(x, n):
+        result += m.comb(n, k) * p**k * (1-p)**(n-k)
+
+    return result
+
+
+def plot():
+    competence_levels = np.linspace(0.1,0.9,20)
+    jury = [x for x in range(3,103)]
+    results = []
+    for p in competence_levels:
+        for i in jury:
+            results.append(calc_cumbinprob(i,p))
+        plt.plot(jury, results)
+        results = []    
+
+    plt.title("Probability of a correct decision")
+    plt.xlabel("Jury size")
+    plt.ylabel("Probability")
+    plt.show()
+
+
+plot()
